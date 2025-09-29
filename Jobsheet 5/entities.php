43e5fa1f16e82107b8e59a9a03fe3da117760838
wawaelenti <irawantielent@gmail.com>
2025-09-29 10:30:14 +0700

    feat: Jobsheet 5

---
+++ Jobsheet 5/entities.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entitas HTML</title>
+</head>
+<body>
+<p>It&#39; time to read a HTML 5 book.</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities:
+Sebuah nama entitas mudah diingat. Kerugian dari menggunakan nama entities:Browser mungkin tidak mendukung semua nama entitas, tetapi 
+dukungan untuk nomor lebih baik.</p>
+<p>&#169; 2023 jti.com</p>
+</body>
+</html>
---
+++ Jobsheet 5/global_server.php
@@ -0,0 +1,6 @@
+<?php
+//menampilan semua informasi yanng tersedia dalam array $_server
+echo '<pra>';
+print_r($_SERVER);
+echo '</pra>';
+?>
